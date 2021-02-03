41f9b5ff753dae0d701b415ecc5f5a69ba558920
student <student@eda-bc-04.localdomain>
2021-02-03 16:41:00 +1300

    add changing background image gradient

--- client/components/GetColourfulLyrics.jsx
+++ client/components/GetColourfulLyrics.jsx
@@ -4,21 +4,24 @@ import SongSearch from './SongSearch'
 
 const getRandomColour = () => `#${Math.floor(Math.random() * 0x1000000).toString(16).padStart(6, 0)}`
 
-
 const GetColour = (props) => {
 
   useEffect(() => {
     const interval = setInterval(() => {
-      setColour(
+      setColour([
+        getRandomColour(),
+        getRandomColour(),
+        getRandomColour(),
+        getRandomColour(),
         getRandomColour()
-      )
+      ])
     }, 800)
     return () => clearInterval(interval)
   }, [])
 
    const [colour, setColour] = useState('none')
   return (
-    <div className="container" style={{ backgroundColor: colour }}>
+    <div className="container" style={{ backgroundImage: `linear-gradient(to bottom right,${colour[0]},${colour[1]},${colour[2]},${colour[3]},${colour[4]})`} }>
       <SongSearch setColour={setColour} getRandomColour={getRandomColour}/>
     </div>
   )
